9f27a351098796a8665d3077f8a9ef773dfdefc7
Kenneth Matovu <matovken@gmail.com>
2015-03-04 09:48:19 +0300

    adjusting for postgres

--- coutries/settings.py
+++ coutries/settings.py
@@ -59,8 +59,13 @@ WSGI_APPLICATION = 'coutries.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'countries',
+        'USER': 'awesome',
+        'HOST': 'localhost',
+        'ATOMIC_REQUESTS': True,
+        'OPTIONS': {
+        }
     }
 }
 
