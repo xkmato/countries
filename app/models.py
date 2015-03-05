345d6d1961f30aec195c863426c63f31d8ba69dd
Kenneth Matovu <matovken@gmail.com>
2015-03-05 11:29:48 +0300

    multilang support

--- app/models.py
+++ app/models.py
@@ -15,3 +15,20 @@ class Country(models.Model):
             return cls.objects.get(name__icontains=string)
         except Exception as e:
             return None
+
+
+class CountryLanguage(models.Model):
+    country = models.ForeignKey(Country, related_name='languages')
+    language = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return '%s, %s, %s' % (self.country.id, self.language, self.name)
+
+    @classmethod
+    def is_valid(cls, string):
+        c = list(cls.objects.filter(name__icontains=string).values_list('country', flat=True).distinct())
+        try:
+            return Country.is_valid(c[0])
+        except Exception as e:
+            return None
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from app.models import Country
+from app.models import Country, CountryLanguage
 
 __author__ = 'awesome'
 
@@ -10,8 +10,11 @@ __author__ = 'awesome'
 def home(request):
     string = request.POST['text']
     country = Country.is_valid(string)
+    c_language = CountryLanguage.is_valid(string)
     if country:
         response = {"valid": "valid", "country": country.name}
+    elif c_language:
+        response = {"valid": "valid", "country": c_language.name}
     else:
         response = {"valid": "invalid"}
     return HttpResponse(json.dumps(response), content_type="application/json")
